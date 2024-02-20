01c5a4dfdb94ec87a8cd9b18d955e3e462379e4d
tatiane <tati.guisso@gmail.com>
2024-02-20 19:23:56 -0300

    first commit

---
+++ src/test/java/com/grupo16/carrinhoservice/CarrinhoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo16.carrinhoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
