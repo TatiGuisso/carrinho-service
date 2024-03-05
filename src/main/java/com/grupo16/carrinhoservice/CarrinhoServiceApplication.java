4b0519b430afeb442d83bfdbd7288d3c575ba3e1
Renan Puppo Furtado <renan.puppo@gmail.com>
2024-03-04 22:56:59 -0300

    Configurando spring cloud

--- src/main/java/com/grupo16/carrinhoservice/CarrinhoServiceApplication.java
+++ src/main/java/com/grupo16/carrinhoservice/CarrinhoServiceApplication.java
@@ -2,8 +2,10 @@ package com.grupo16.carrinhoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarrinhoServiceApplication {
 
 	public static void main(String[] args) {
