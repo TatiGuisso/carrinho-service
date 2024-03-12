88fe42bacbe4c2c2e48b85276c64ae793d83c2f5
Renan Puppo Furtado <renan.puppo@gmail.com>
2024-03-12 20:44:16 -0300

    Fix tratamento de erros

--- src/main/java/com/grupo16/carrinhoservice/gateway/http/produto/ProdutoServiceOpenFeignGateway.java
+++ src/main/java/com/grupo16/carrinhoservice/gateway/http/produto/ProdutoServiceOpenFeignGateway.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.grupo16.carrinhoservice.domain.Item;
 import com.grupo16.carrinhoservice.exception.ErrorAoAcessarProdutoServiceException;
+import com.grupo16.carrinhoservice.exception.SystemBaseException;
+import com.grupo16.carrinhoservice.exception.SystemExternalException;
 import com.grupo16.carrinhoservice.gateway.ProdutoServiceGateway;
 import com.grupo16.carrinhoservice.gateway.http.produto.json.ProdutoJson;
 
@@ -20,34 +23,40 @@ import lombok.extern.slf4j.Slf4j;
 public class ProdutoServiceOpenFeignGateway implements ProdutoServiceGateway {
 
 	private ProdutoServiceFeignClient produtoServiceFeignClient;
-	
+
+	private ObjectMapper objectMapper;
+
 	@Override
 	public List<Item> obterPorId(List<Long> ids) {
-	try {
-		
-		List<Item> itens = new ArrayList<>();
-		
-		ids.stream().forEach(id -> {
-			ProdutoJson produtoJson = produtoServiceFeignClient.obterPorId(id);
-			itens.add(Item.builder()
-					.idProduto(produtoJson.getId())
-					.precoUnitario(produtoJson.getPrecoUnitario())
-					.build());
-		});
-		
+		try {
 
-		return itens;
-	} catch (Exception e) {
-		log.error(e.getMessage(), e);
-		if(e instanceof FeignException feignException) {
+			List<Item> itens = new ArrayList<>();
 
-			//TODO: implementar
-			String exceptionResponseBody = feignException.contentUTF8();
-			log.error(exceptionResponseBody);
+			ids.stream().forEach(id -> {
+				ProdutoJson produtoJson = produtoServiceFeignClient.obterPorId(id);
+				itens.add(Item.builder()
+						.idProduto(produtoJson.getId())
+						.precoUnitario(produtoJson.getPrecoUnitario())
+						.build());
+			});
 
+
+			return itens;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			if(e instanceof FeignException feignException) {
+				try {
+					String exceptionResponseBody = feignException.contentUTF8();
+					SystemBaseException systemBaseException = objectMapper.readValue(exceptionResponseBody, SystemExternalException.class);
+					throw systemBaseException;
+
+				} catch (Exception e2) {
+					log.error(e2.getMessage(), e);
+					throw new ErrorAoAcessarProdutoServiceException();
+				}
+			}
+			throw new ErrorAoAcessarProdutoServiceException();
 		}
-		throw new ErrorAoAcessarProdutoServiceException();
 	}
-}
 
 }
